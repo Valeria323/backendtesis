a8fbc507ac07589964fbd6bb12d1d382449fb36f
Valeria Dominguez <Valukadominguez@gmail.com>
2022-06-24 17:17:02 -0500

    update MarcaService

--- serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/MarcaService.java
+++ serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/MarcaService.java
@@ -3,8 +3,12 @@ package ec.edu.insteclrg.service.crud;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ec.edu.insteclrg.common.exception.ResourceNotFoundException;
 import ec.edu.insteclrg.domain.Marca;
+import ec.edu.insteclrg.domain.Test;
 import ec.edu.insteclrg.dto.MarcaDTO;
+import ec.edu.insteclrg.dto.TestDTO;
 import ec.edu.insteclrg.persistence.MarcaRepository;
 import ec.edu.insteclrg.service.GenericCrudServiceImpl;
 
@@ -34,5 +38,18 @@ public class MarcaService extends GenericCrudServiceImpl<Marca, MarcaDTO> {
 		marca.setNombre(dto.getNombre());
 		return marca;
 	}
+	
+	public void update(Long id, MarcaDTO dto) {
+		MarcaDTO marcaDto = new MarcaDTO();
+		marcaDto.setId(id);
+		Optional<Marca> optionalmarca = repository.findById(marcaDto.getId());
+		if (!optionalmarca.isPresent()) {
+			throw new ResourceNotFoundException(String.format("El id %s no se encuentra registrado", id));
+		}
+		Marca marca = optionalmarca.get();
+		marca.setNombre(dto.getNombre());
+		repository.save(marca);
 
 }
+}
+
