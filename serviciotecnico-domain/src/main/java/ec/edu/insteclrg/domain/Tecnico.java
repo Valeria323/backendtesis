e5972e1d4c29cfb6df7b5336849bbe2e4e456976
Valeria Tigre <valeria.tigre@isteclrg.edu.ec>
2022-06-28 19:37:30 -0500

    Tecnico entity created

---
+++ serviciotecnico-domain/src/main/java/ec/edu/insteclrg/domain/Tecnico.java
@@ -0,0 +1,34 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Tecnico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+    private Provincia cedula;
+	
+	@Column(nullable = false)
+	private String nombre; 
+	
+	@Column(nullable = false)
+    private String apellido;
+	
+	@Column(nullable = false)
+    private String email;
+	
+	@Column(nullable = false)
+    private String telefono;
+	
+	@Column(nullable = false)
+    private String direccion;
+	
+	
+	/*idciudad*/
+}
