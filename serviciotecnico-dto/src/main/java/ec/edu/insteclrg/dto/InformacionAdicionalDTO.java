ec54abc6a4e44316dec446c0beae24783c1596e9
Valeria Dominguez <Valukadominguez@gmail.com>
2022-07-08 16:35:01 -0500

    created InformacionAdicional: service, controller, dto, repository

---
+++ serviciotecnico-dto/src/main/java/ec/edu/insteclrg/dto/InformacionAdicionalDTO.java
@@ -0,0 +1,16 @@
+package ec.edu.insteclrg.dto;
+
+import lombok.Data;
+
+@Data
+public class InformacionAdicionalDTO {
+
+	private long id;
+
+	private String nombre;
+
+	private String descripcion;
+
+	public String getNombre;
+	
+}
---
+++ serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/InformacionAdicionalService.java
@@ -0,0 +1,37 @@
+package ec.edu.insteclrg.service.crud;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ec.edu.insteclrg.domain.InformacionAdicional;
+import ec.edu.insteclrg.dto.InformacionAdicionalDTO;
+import ec.edu.insteclrg.persistence.InformacionAdicionalRepository;
+import ec.edu.insteclrg.service.GenericCrudServiceImpl;
+
+@Service
+public class InformacionAdicionalService extends GenericCrudServiceImpl<InformacionAdicional, InformacionAdicionalDTO>{
+
+	@Autowired
+	private InformacionAdicionalRepository repository;
+
+	private ModelMapper modelMapper = new ModelMapper();
+
+	@Override
+	public Optional<InformacionAdicional> find(InformacionAdicionalDTO dto) {
+		return repository.findById(dto.getId());
+	}
+
+	@Override
+	public InformacionAdicionalDTO mapToDto(InformacionAdicional domain) {
+		return modelMapper.map(domain, InformacionAdicionalDTO.class);
+	}
+
+	@Override
+	public InformacionAdicional mapToDomain(InformacionAdicionalDTO dto) {
+		return modelMapper.map(dto, InformacionAdicional.class);
+	}
+	
+}
