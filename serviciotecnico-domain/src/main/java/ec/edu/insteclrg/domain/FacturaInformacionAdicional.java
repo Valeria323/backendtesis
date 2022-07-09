ae3e0e2022777445973a91da0e5719292f2424f6
ValeriaTigre <valeria.tigre@insteclrg.edu.ec>
2022-07-09 10:24:42 -0500

    FacturaInformacionAdicional entity,DTO,Repository,service,controller created

---
+++ serviciotecnico-domain/src/main/java/ec/edu/insteclrg/domain/FacturaInformacionAdicional.java
@@ -0,0 +1,36 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+public class FacturaInformacionAdicional {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "facture_id")
+	private Factura factura;
+
+	@Column(nullable = false, unique = true)
+	private String nombre;
+
+	@Column(nullable = false)
+	private String descripcion;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_empresa")
+	private Empresa empresa;
+	
+	
+}
