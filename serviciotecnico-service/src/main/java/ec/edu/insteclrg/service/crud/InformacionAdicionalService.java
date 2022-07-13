81b340de7fd3110b63ee30ef96d46d3c61a10fd3
ValeriaTigre <valeria.tigre@insteclrg.edu.ec>
2022-07-13 17:35:26 -0500

    InformacionAdicional entity,service,controller Update

--- serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/InformacionAdicionalService.java
+++ serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/InformacionAdicionalService.java
@@ -1,11 +1,9 @@
 package ec.edu.insteclrg.service.crud;
 
 import java.util.Optional;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import ec.edu.insteclrg.domain.InformacionAdicional;
 import ec.edu.insteclrg.dto.InformacionAdicionalDTO;
 import ec.edu.insteclrg.persistence.InformacionAdicionalRepository;
@@ -33,5 +31,4 @@ public class InformacionAdicionalService extends GenericCrudServiceImpl<Informac
 	public InformacionAdicional mapToDomain(InformacionAdicionalDTO dto) {
 		return modelMapper.map(dto, InformacionAdicional.class);
 	}
-	
 }
