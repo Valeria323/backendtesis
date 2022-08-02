69549cc4ce24d8553db84bbee8506d69247687a6
ValeriaTigre <valeria.tigre@insteclrg.edu.ec>
2022-08-02 17:32:06 -0500

    Update Delete TecnicoService

--- serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/TecnicoService.java
+++ serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/TecnicoService.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ec.edu.insteclrg.common.exception.ResourceNotFoundException;
 import ec.edu.insteclrg.domain.Tecnico;
 import ec.edu.insteclrg.dto.TecnicoDTO;
 import ec.edu.insteclrg.persistence.TecnicoRepository;
@@ -30,5 +32,15 @@ public class TecnicoService extends GenericCrudServiceImpl<Tecnico, TecnicoDTO> 
 	@Override
 	public Tecnico mapToDomain(TecnicoDTO dto) {
 		return modelMapper.map(dto, Tecnico.class);
+	}
+	public void delete(TecnicoDTO dto) {
+
+		Optional<Tecnico> optional = repository.findById(dto.getId());
+
+		if (!optional.isPresent()) {
+			throw new ResourceNotFoundException(String.format("Registro %s no existe en la base de datos", dto));
+		}
+		Tecnico tecnico = optional.get();
+		repository.delete(tecnico);
 	}
 }
