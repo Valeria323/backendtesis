0c7bcd28bb097a3e059206e6d6a062c04f9e2960
Valeria323 <98496327+Valeria323@users.noreply.github.com>
2022-08-02 17:20:57 -0500

    udpate Delete ProductoService

--- serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/ProductoService.java
+++ serviciotecnico-service/src/main/java/ec/edu/insteclrg/service/crud/ProductoService.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ec.edu.insteclrg.common.exception.ResourceNotFoundException;
 import ec.edu.insteclrg.domain.Producto;
 import ec.edu.insteclrg.dto.ProductoDTO;
 import ec.edu.insteclrg.persistence.ProductoRepository;
@@ -27,9 +28,6 @@ public class ProductoService extends GenericCrudServiceImpl<Producto, ProductoDT
 	@Override
 	public ProductoDTO mapToDto(Producto domain) {
 		ProductoDTO productoDTO = new ProductoDTO();
-		// productoDTO.setId(domain.getId());
-		// productoDTO.setCodigo(domain.getCodigo());
-		// productoDTO.setNombre(domain.getNombre());
 		productoDTO = modelMapper.map(domain, ProductoDTO.class);
 		return productoDTO;
 	}
@@ -37,11 +35,17 @@ public class ProductoService extends GenericCrudServiceImpl<Producto, ProductoDT
 	@Override
 	public Producto mapToDomain(ProductoDTO dto) {
 		Producto producto = new Producto();
-		// producto.setId(dto.getId());
-		// producto.setCodigo(dto.getCodigo());
-		// producto.setNombre(dto.getNombre());
 		producto = modelMapper.map(dto, Producto.class);
 		return producto;
 	}
 	
+	public void delete(ProductoDTO dto) {
+		Optional<Producto> optional = repository.findById(dto.getId());
+		if (!optional.isPresent()) {
+			throw new ResourceNotFoundException(String.format("Registro %s no existe en la base de datos", dto));
+		}
+		Producto producto = optional.get();
+		repository.delete(producto);
+
+	}
 }
